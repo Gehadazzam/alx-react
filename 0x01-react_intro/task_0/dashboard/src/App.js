891091432f97d80f60af21c310fe19e3d496671e
Gehadazzam <gehadgamalazzam@gmail.com>
2024-07-27 16:02:16 +0300

    First React App

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import logo from "./logo.jpg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>School dashboard</p>
+      </header>
+      <main className="App-body">
+        <p>Login to access the full dashboard</p>
+      </main>
+      <footer className="App-footer">
+        <p>Copyright 2020 - Holberton School</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
