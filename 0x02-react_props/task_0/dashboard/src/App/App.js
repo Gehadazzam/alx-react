d103130021f2a26aa6c0f4e82caa8d578f7865e5
Gehadazzam <gehadgamalazzam@gmail.com>
2024-07-29 08:58:42 +0300

    start dividing to props

---
+++ 0x02-react_props/task_0/dashboard/src/App/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import displayHEader from "../Header/Header";
+function App() {
+  return (
+    <div className="App">
+      displayHEader()
+      <main className="App-body">
+        <p>Login to access the full dashboard</p>
+      </main>
+      <footer className="App-footer">
+        <p>Copyright 2020 - Holberton School</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ 0x02-react_props/task_0/dashboard/src/Header/Header.js
@@ -0,0 +1,12 @@
+import "./Header.css";
+function displayHEader() {
+  return (
+    <>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>School dashboard</p>
+      </header>
+    </>
+  );
+}
+export default displayHEader;
