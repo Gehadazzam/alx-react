96c14954a19b874a4495ec0aa11880c66c0c9ac6
Gehadazzam <gehadgamalazzam@gmail.com>
2024-08-22 19:17:27 +0300

    [Task 4]: Create actions for the UI

---
+++ 0x07-react_redux_action_creator_normalizr/task_4/dashboard/src/App/AppContext.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const defaultUser = {
+  email: "",
+  password: "",
+  isLoggedIn: false,
+};
+
+export const defaultLogout = () => {};
+export const AppContext = React.createContext({
+  defaultUser,
+  defaultLogout,
+});
