858456269614b628e64d775da382e371dc34a608
Gehadazzam <gehadgamalazzam@gmail.com>
2024-09-03 18:30:37 +0300

    [Task 16]: Combine store test

--- 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/App.test.js
+++ 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/App.test.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { shallow } from "enzyme";
 import App from "./App";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./reducers/rootReducer"; // Assuming rootReducer is the combined reducer
 
 describe("App Component", () => {
   it("renders correctly", () => {
-    const wrapper = shallow(<App />);
+    const store = createStore(rootReducer);
+    const wrapper = shallow(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
     expect(wrapper).toMatchSnapshot();
   });
 });
--- 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/reducers/uiReducer.test.js
+++ 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/reducers/uiReducer.test.js
@@ -1,22 +1,21 @@
-import { uiReducer, initialState } from "./uiReducer";
-import { TOGGLE_NOTIFICATION_DRAWER } from "../actions/uiActionTypes";
+import rootReducer from "./rootReducer"; // Import the rootReducer instead of uiReducer
+import { initialState as uiInitialState } from "./uiReducer"; // Import initialState from uiReducer
+import { initialState as courseInitialState } from "./courseReducer"; // Import initialState from courseReducer
+import { initialState as notificationInitialState } from "./notificationReducer"; // Import initialState from notificationReducer
 import { DISPLAY_NOTIFICATION_DRAWER } from "../actions/uiActionTypes"; // Add this import
 
-describe("uiReducer", () => {
+describe("rootReducer", () => {
   it("should return the initial state when no action is passed", () => {
-    expect(uiReducer(undefined, {})).toEqual(initialState);
+    const expectedInitialState = {
+      courses: courseInitialState,
+      notifications: notificationInitialState,
+      ui: uiInitialState,
+    };
+    expect(rootReducer(undefined, {})).toEqual(expectedInitialState);
   });
 
   it("should change isNotificationDrawerVisible to true when DISPLAY_NOTIFICATION_DRAWER action is passed", () => {
-    const state = uiReducer(undefined, { type: DISPLAY_NOTIFICATION_DRAWER });
-    expect(state.get("isNotificationDrawerVisible")).toBe(true);
-  });
-
-  it("should toggle isNotificationDrawerVisible when TOGGLE_NOTIFICATION_DRAWER action is passed", () => {
-    const initialState = Map({ isNotificationDrawerVisible: false });
-    const newState = uiReducer(initialState, {
-      type: TOGGLE_NOTIFICATION_DRAWER,
-    });
-    expect(newState.get("isNotificationDrawerVisible")).toBe(true);
+    const state = rootReducer(undefined, { type: DISPLAY_NOTIFICATION_DRAWER });
+    expect(state.ui.get("isNotificationDrawerVisible")).toBe(true);
   });
 });
